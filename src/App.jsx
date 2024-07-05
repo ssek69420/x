fbb2ca08e0aee18d78b4315602baf7ef299849db
ssek69420 <kaikevix2233@gmail.com>
2024-07-05 14:27:58 -0400

    fiz
    ahahaha

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "../src/globals.css";
+import "react-toastify/dist/ReactToastify.css"
+import { ToastContainer, toast} from "react-toastify";
 
 export default function App() {
 
@@ -12,13 +14,13 @@ export default function App() {
         },
         {
             id: 2,
-            item: "Coca-cola 350ml",
+            item: "Coca-Cola 350ml",
             imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm4S15squn95k7qtrVOpMX1MOJGe48y4B7FQ&s",
             preco: "R$ 5,99"
         },
         {
             id: 3,
-            item: "Batatas fritas",
+            item: "Batata Frita",
             imagem: "https://gastronomiacarioca.zonasul.com.br/wp-content/uploads/2023/05/batata_frita_destaque_ilustrativo_zona_sul.jpg",
             preco: "R$ 8,99"
         },
@@ -33,11 +35,25 @@ export default function App() {
     const [listaPedidos, setListaPedidos] = useState([]);
 
     const adicionarPedido = (item) => {
+        toast("Pedido adicionado.")
         setListaPedidos([...listaPedidos, item]);
     }
 
     const removerPedido = (id) => {
-        let listaAux = listaPedidos.filter((produto)=> produto.id !== id);
+        let removeu = false
+        let listaAux = listaPedidos.filter((produto)=> {
+            if (removeu == false){
+                if (produto.id !== id){
+                    return produto
+                }else{
+                    toast(`Você removeu ${produto.item}`);
+                    removeu = true;
+                    return null
+                }
+            }else{
+                return produto
+            }
+        });
         setListaPedidos(listaAux);
     }
 
@@ -74,6 +90,7 @@ export default function App() {
                     </table>
                 )}
             </div>
+            <ToastContainer/>
         </div>
     );
 }
